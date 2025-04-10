85cf9d0d9e7740dcdf70680c97138a7f719c07d3
Rez-Aik <shevchukaleksei1999@yandex.ru>
2025-04-10 18:09:05 +0300

    It works

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Alex","Shev", (byte) 26);
+        userServiceImpl.saveUser("Dima","Shep", (byte) 29);
+        userServiceImpl.saveUser("Artem","Sark", (byte) 28);
+        userServiceImpl.saveUser("Masha","Kuz", (byte) 25);
+        userServiceImpl.getAllUsers().forEach(System.out::println);
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
